28939b220b51328d29d62e228ce3285a754a3d16
JayPrakashThakur <jayprakashthakursnr@gmail.com>
2018-06-21 10:04:43 +0530

    Added multiply_left_and_right_array_sum

---
+++ practice/school/multiply_left_and_right_array_sum.py
@@ -0,0 +1,39 @@
+"""
+Pitsy needs help in the given task by her teacher. The task is to divide a array into two sub array (left and right) containing n/2 elements each and do the sum of the subarrays and then multiply both the subarrays.
+
+Input:
+First line consists of T test cases. Only line of every test case consists of an integer N.​
+
+Output:
+Print the answer by dividing array into left and right array and add their elements individually and then multiply both the array
+
+Constraints:
+1<=T<=100
+1<=N<=1000
+1<=Ai<=100
+
+Example:
+Input:
+2
+4
+1 2 3 4
+3
+4 5 6
+
+Output:
+21
+44
+"""
+
+
+def multiply_left_and_right_array_sum(arr, size):
+    l1, l2 = arr[:size // 2], arr[size // 2:]
+    return int(sum(l1) * sum(l2))
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        size = int(input())
+        arr = [float(i) for i in input().split()][0:size]
+        print(multiply_left_and_right_array_sum(arr, size))
