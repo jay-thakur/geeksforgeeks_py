653117d315999cc9864e91db23e5ff3ce117a687
jayprakashthakursnr@gmail.com <jayprakashthakursnr@gmail.com>
2019-01-01 20:08:51 +0530

    Added repetitive_addition_of_digits

---
+++ practice/Basic/repetitive_addition_of_digits.py
@@ -0,0 +1,38 @@
+"""
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Input:
+The first line contains 'T' denoting the number of testcases. Then follows description of testcases. The next T lines contains a single integer N denoting the value of N.
+
+Output:
+Output the sum of all its digit until the result has only one digit.
+
+Constraints:
+1<=T<=30
+1<=n<=10^9
+
+Example:
+Input :
+2
+1
+98
+
+Output :
+1
+8
+
+Explanation:
+For example, if we consider 98, we get 9+8  = 17 after first addition. Then we get 1+7 = 8.
+We stop at this point because we are left with one digit.
+"""
+
+
+def repetitive_addition_of_digits(n):
+    return n % 9
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        print(repetitive_addition_of_digits(n))
